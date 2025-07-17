fb6990b46d564c8b55058ee0136353d272b20d26
komidan <komidan.gg@gmail.com>
2025-07-17 17:08:12 -0400

    clang lsp fix causing bad indentation, added QOL/visual settings to vim-settings.lua

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,36 @@
 return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
+    branch = 'master',
+    lazy = false,
+
     config = function()
         local config = require("nvim-treesitter.configs")
         config.setup({
-            ensure_installed = {"lua", "javascript", "python", "c", "vim", "html"},
-            highlight = { enable = true },
-            indent = { enable = true },
+            ensure_installed = {
+                -- langs
+                "c",
+                "python",
+                "lua",
+
+                -- vim
+                "vim",
+                "vimdoc",
+
+                -- qol
+                "query",
+                "markdown",
+                "markdown_inline",
+            },
+            highlight = {
+                enable = true,
+                disable = { "c", "rust" },
+
+                additional_vim_regex_highlighting = false,
+            },
+            indent = {
+                enable = false,
+            },
         })
     end
 }
--- nvim/lua/vim-settings.lua
+++ nvim/lua/vim-settings.lua
@@ -1,9 +1,9 @@
--- mem
+-- files 
 vim.opt.backup = false
 vim.opt.undofile = true
 vim.opt.swapfile = false
 
--- tabbings
+-- indentation
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
@@ -11,20 +11,35 @@ vim.opt.expandtab = true
 vim.opt.autoindent = true
 vim.opt.smartindent = true
 
--- gui
+-- visual 
 vim.opt.nu = true
 vim.opt.relativenumber = true
+vim.api.nvim_set_hl(0, "CursorLineNr", { bg="#222627" })
 vim.opt.signcolumn = "yes:1"
 vim.opt.wrap = true
 vim.opt.background = "dark"
 vim.opt.termguicolors = true
 vim.opt.colorcolumn = { 80 }
-vim.cmd("hi ColorColumn guibg=#212526")
+vim.api.nvim_set_hl(0, "ColorColumn", { bg="#222627" })
+vim.opt.showmatch = true
+vim.opt.matchtime = 2
+vim.opt.cursorline = true
+vim.api.nvim_set_hl(0, "CursorLine", { bg="#222627" })
 
 -- qol 
 vim.opt.incsearch = true
 vim.opt.scrolloff = 24
-vim.opt.updatetime = 50 -- (swapfile is off)
+vim.opt.updatetime = 100 -- (swapfile is off)
+vim.opt.lazyredraw = true
+vim.opt.timeoutlen = 100
+vim.opt.ttimeoutlen = 0
+vim.opt.autoread = true
+vim.opt.autowrite = false
+vim.opt.iskeyword:append("-")
+vim.opt.hidden = true
+vim.opt.errorbells = false
+vim.opt.selection = "exclusive"
+vim.opt.clipboard:append("unnamedplus")
 
 -- keybindings
 vim.g.mapleader = " "
