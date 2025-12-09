35a94b36dbef55578abed551623041882f637ec5
komidan <komidan.gg@gmail.com>
2025-12-08 20:46:37 -0500

    a

--- nvim/lua/lsp/pylsp.lua
+++ nvim/lua/lsp/pylsp.lua
@@ -7,10 +7,19 @@ vim.lsp.config["pylsp"] = {
                 pycodestyle = {
                     enabled = true,
                     ignore = {
+<<<<<<< HEAD
                         "E501", "E305", "E302", "E261", "E221", "E303",
                         "E265", "E301", "E241"
                     }
                 }
+=======
+                        "E501", "E305", "E302", "E261",
+                        "E221", "E303", "E265", "E251",
+                    },
+                    maxLineLength = 120,
+                 } ,
+               pyflakes = { enabled = true },
+>>>>>>> 96d8258 (more changes)
             }
         }
     }
